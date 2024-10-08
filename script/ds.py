e37936442bca971e70bed010390a989a49c4c2f3
Claudio Santos <claudio.santos@cordeiro.com.br>
2024-10-08 15:03:31 -0300

    08/10 work

--- script/ds.py
+++ script/ds.py
@@ -6,21 +6,17 @@ import plotly.express as px
 import os
 
 # Definir o diretório base como o caminho do próprio script
+# Definir o diretório base como o caminho do próprio script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Defina os caminhos dos arquivos usando caminhos relativos
 DADOS_POINTING_PATH = os.path.join(BASE_DIR, '.database', 'DATABASE.xlsx')
-DADOS_MONITORING_PATH = os.path.join(BASE_DIR, '.database', 'ACOMPANHAMENTO.xlsx')
-DADOS_DEMAND_PATH = os.path.join(BASE_DIR, '.database', 'ACOMPANHAMENTO.xlsx')
+DADOS_MONITORING_PATH = os.path.join(BASE_DIR, '.database', 'ACOMPANHAMENTO DE PRODUÇÃO ATUAL-.xlsx')  # Ajustado para incluir a barra invertida correta
+DADOS_DEMAND_PATH = os.path.join(BASE_DIR, '.database', 'ACOMPANHAMENTO DE PRODUÇÃO ATUAL-.xlsx')  # Ajustado para incluir a barra invertida correta
 
 @st.cache_data
 def carregar_todos_os_dados():
-    with st.spinner("""
-                    ######
-                    #### DASHBOARD OPERACIONAL
-                    ###### Carregando os dados...
-                    ###### Por favor aguarde...
-                    """):
+    with st.spinner():
         dados_monitoring = carregar_dados_monitoring()
         dados_pointing = carregar_dados_pointing()
         dados_demand = carregar_dados_demand()
@@ -83,48 +79,61 @@ def pagina1(dados_monitoring):
 def pagina2(dados_pointing):
     st.write('## Pointing')
     st.write('#### Acompanhamento de produção')
-    
-    if dados_pointing is not None:
-        # Obter anos e meses únicos dos dados
-        anos = dados_pointing['Ano'].unique()
-        meses = dados_pointing['Mês'].unique()
 
-        # Filtros para ano e mês
-        ano_selecionado = st.multiselect('Selecione o(s) Ano(s)', anos)
-        mes_selecionado = st.multiselect('Selecione o(s) Mês(es)', meses)
+    # Carregar os dados
+    dados = carregar_dados_pointing()
+
+    if dados is not None:
+        # Filtros
+        anos = dados['Ano'].unique()
+        meses = dados['Mês'].unique()
+
+        # Utilizando session_state para manter a seleção dos filtros
+        if 'anos_selecionados' not in st.session_state:
+            st.session_state.anos_selecionados = anos.tolist()  # Selecionar todos por padrão
+        if 'meses_selecionados' not in st.session_state:
+            st.session_state.meses_selecionados = meses.tolist()  # Selecionar todos por padrão
+
+        ano_selecionado = st.multiselect('Selecione o(s) Ano(s)', anos, default=st.session_state.anos_selecionados)
+        mes_selecionado = st.multiselect('Selecione o(s) Mês(es)', meses, default=st.session_state.meses_selecionados)
+
+        # Atualizar o estado com a seleção atual
+        st.session_state.anos_selecionados = ano_selecionado
+        st.session_state.meses_selecionados = mes_selecionado
 
         # Filtrar dados por ano e mês
         if ano_selecionado and mes_selecionado:
-            dados_filtrados = dados_pointing[
-                (dados_pointing['Ano'].isin(ano_selecionado)) & 
-                (dados_pointing['Mês'].isin(mes_selecionado))
-            ]
+            dados_filtrados = dados[(dados['Ano'].isin(ano_selecionado)) & (dados['Mês'].isin(mes_selecionado))]
 
             if not dados_filtrados.empty:
+                # Exibir dados filtrados em uma lista expansível (toggle list)
                 with st.expander(f'Exibir Dados Filtrados para os Anos {ano_selecionado} e Meses {mes_selecionado}'):
                     st.dataframe(dados_filtrados)
 
+                # Verificar se as colunas 'Produção Cobre Realizado' e 'Produção Alumínio Realizado' existem
                 if 'Produção Cobre Realizado' in dados_filtrados.columns and 'Produção Alumínio Realizado' in dados_filtrados.columns:
+                    # Somar as colunas 'Produção Cobre Realizado' e 'Produção Alumínio Realizado'
                     total_cobre = dados_filtrados['Produção Cobre Realizado'].sum()
                     total_aluminio = dados_filtrados['Produção Alumínio Realizado'].sum()
 
+                    # Exibir métricas de Produção Cobre e Produção Alumínio
                     col1, col2 = st.columns(2)
                     with col1:
-                        st.metric(label="Produção de Cobre", value=f"{total_cobre:.2f}")
-
+                        st.metric(label="Cobre", value=f"{total_cobre:.2f}")
                     with col2:
-                        st.metric(label="Produção de Alumínio", value=f"{total_aluminio:.2f}")
+                        st.metric(label="Alumínio", value=f"{total_aluminio:.2f}")
 
                     # Agrupar por 'Ano' e 'Mês' e somar as colunas de produção
                     dados_filtrados_grouped = dados_filtrados.groupby(['Ano', 'Mês'])[['Produção Cobre Realizado', 'Produção Alumínio Realizado']].sum()
 
+                    # Gráfico de Linhas
                     st.write("### Gráfico de Linhas")
-                    fig_line = px.line(dados_filtrados_grouped.reset_index(), x='Mês', 
-                                       y=['Produção Cobre Realizado', 'Produção Alumínio Realizado'], 
+                    fig_line = px.line(dados_filtrados_grouped.reset_index(), x='Mês', y=['Produção Cobre Realizado', 'Produção Alumínio Realizado'], 
                                        labels={'value': 'Produção', 'index': 'Ano-Mês'}, 
                                        title="Evolução da Produção Realizada (Cobre vs Alumínio)")
                     st.plotly_chart(fig_line)
 
+                    # Gráfico de Barras
                     st.write("### Gráfico de Barras")
                     fig_bar = px.bar(dados_filtrados_grouped.reset_index(), x='Mês', 
                                      y=['Produção Cobre Realizado', 'Produção Alumínio Realizado'], 
@@ -132,6 +141,7 @@ def pagina2(dados_pointing):
                                      title="Produção Realizada Agregada (Cobre vs Alumínio)")
                     st.plotly_chart(fig_bar)
 
+                    # Gráfico de Pizza (Proporção)
                     st.write("### Gráfico de Setores")
                     proporcoes = pd.DataFrame({
                         'Material': ['Cobre', 'Alumínio'],
@@ -148,7 +158,6 @@ def pagina2(dados_pointing):
             st.write("Selecione pelo menos um ano e um mês para comparar os dados.")
     else:
         st.write("Erro ao carregar os dados.")
-
 def pagina3(dados_demand):
     st.write('## Demand')
     st.write('#### Relevância por composto')
@@ -173,10 +182,10 @@ if 'pagina_atual' not in st.session_state:
     st.session_state.pagina_atual = 'pagina1'  # Página inicial
 
 botao_pagina1 = st.sidebar.button('Página 1 (ICON)', on_click=lambda: st.session_state.update({'pagina_atual': 'pagina1'}))
-st.sidebar.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
 
 botao_pagina2 = st.sidebar.button('Página 2 (ICON)', on_click=lambda: st.session_state.update({'pagina_atual': 'pagina2'}))
-st.sidebar.markdown("<br><br><br><br><br><br>", unsafe_allow_html=True)
+st.sidebar.markdown("<br><br><br><br><br>", unsafe_allow_html=True)
 
 botao_pagina3 = st.sidebar.button('Página 3 (ICON)', on_click=lambda: st.session_state.update({'pagina_atual': 'pagina3'}))
 # Carregar os dados
